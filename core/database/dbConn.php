a95a704a762ce9cf35eb74c6958463746b6206e7
Dale <yl622@njit.edu>
2017-12-03 23:18:21 -0500

    Copied dbConn in classes from my active record homework and made some small changes.

---
+++ core/database/dbConn.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dale
+ * Date: 12/3/2017
+ * Time: 11:11 PM
+ */
+    namespace core\database;
+    class dbConn{
+        protected static $db;
+        private function __construct() {
+            global $connErr;
+            try {
+                self::$db = new \PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+                self::$db->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
+            }
+            catch (\PDOException $e) {
+                $connErr .= htmlTags::changeRow("Connection Error: " . $e->getMessage());
+            }
+        }
+        public static function getConnection() {
+            if (!self::$db) {
+                new dbConn();
+            }
+            return self::$db;
+        }
+    }
+?>
