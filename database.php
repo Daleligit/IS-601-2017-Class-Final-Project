e171714a227410fc68bb11247507b08b5589d015
Dale <yl622@njit.edu>
2017-12-05 21:32:35 -0500

    Created database.php

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dale
+ * Date: 12/5/2017
+ * Time: 9:31 PM
+ */
+    define('DATABASE', 'yl622');
+    define('USERNAME', 'yl622');
+    define('PASSWORD', 'evPkHDGVf');
+    define('CONNECTION', 'sql2.njit.edu');
+?>
