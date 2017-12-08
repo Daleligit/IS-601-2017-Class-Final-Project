e3c15c093de3c7fc309003bcd85fd9c29f6a3cdd
Dale <yl622@njit.edu>
2017-12-07 21:03:28 -0500

    Added header and footer in pages

---
+++ pages/footer.php
@@ -0,0 +1,9 @@
+/**
+ * Created by PhpStorm.
+ * User: Dale
+ * Date: 12/7/2017
+ * Time: 9:02 PM
+ */
+<script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,24 @@
+/**
+ * Created by PhpStorm.
+ * User: Dale
+ * Date: 12/7/2017
+ * Time: 9:01 PM
+ */
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="styles.css">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
