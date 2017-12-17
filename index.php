0d118f049809815e9987595d25109e96f79f8701
Dale <yl622@njit.edu>
2017-12-16 21:51:07 -0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dale
+ * Date: 12/16/2017
+ * Time: 9:49 PM
+ */
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    include_once "autoload.php";
+    include_once "database.php";
+    $response = core\http\processRequest::createResponse();
+?>
